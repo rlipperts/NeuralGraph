a12d4891f44b1de5301f4e495db5b844354a40fc
Ruben Lipperts <rlipperts@techfak.uni-bielefeld.de>
2018-07-16 11:31:36 +0200

    dummy compiler class added

---
+++ compiler/src/main/java/Compiler.java
@@ -0,0 +1,16 @@
+import Model.Graph.Graph;
+
+public class Compiler {
+
+    /**
+     * Starts compilation
+     * @param graph Graph from which to build the neural net
+     * @return Errors or return messages
+     */
+    public String compile(Graph graph) {
+
+        return null;
+    }
+
+
+}
